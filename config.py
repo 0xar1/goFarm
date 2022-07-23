ba10b040b2d093b3f0474f327a3c2813c33bf1fd
arwell <41910892+0xar1@users.noreply.github.com>
2022-07-23 02:27:48 -0700

    added new urllib password column

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
-
+from urllib.parse import quote
 class Config(object):
      SECRET_KEY = os.environ.get('SECRET_KEY') or 'i123he98skdq'
      # MYSQL_USER = 'root'
      # MYSQL_PASSWORD = 'passwordroot691'
      # MYSQL_DB = 'app'
      # MYSQL_HOST = 'localhost'
-     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:password69@localhost/app'
+     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:%s@localhost/app' % quote('password69')
      SQLALCHEMY_TRACK_MODIFICATIONS = False
